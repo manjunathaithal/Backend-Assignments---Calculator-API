75d6141fb5933decb139bcd1f795e4ecd27da2bd
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-03 15:27:24 +0530

    Added Get Request

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 // your code goes here
-app.get("hello World!");
+app.get("/home", (req, res) => {
+  res.send("Hello world!");
+});
 app.post("/add", (req, res) => {
   let result = req.body.num1 + req.body.num2;
   res.send({
