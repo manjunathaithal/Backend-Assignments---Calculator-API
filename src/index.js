4ef4e12c936aa543db834b2171f2122cea72384c
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-03 15:21:29 +0530

    adding

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const { json } = require("express");
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
+app.post("/add", (req, res) => {
+  let result = req.body.num1 + req.body.num2;
+  res.send({
+    status: "success",
+    message: "the sum of given two numbers",
+    sum: result,
+  });
+});
+app.post("/sub", (req, res) => {
+  let result = req.body.num1 - req.body.num2;
+  res.send({
+    status: "success",
+    message: "the difference of given two numbers",
+    difference: result,
+  });
+});
+app.post("/multiply", (req, res) => {
+  let result = req.body.num1 * req.body.num2;
+  res.send({
+    status: "success",
+    message: "The product of given numbers",
+    result: result,
+  });
+});
 
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.post("/divide", (req, res) => {
+  let result = req.body.num1 / req.body.num2;
+  res.send({
+    status: "success",
+    message: "The division of given numbers",
+    result: result,
+  });
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
